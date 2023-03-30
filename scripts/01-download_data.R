49af19babea2fa0f52ffca302a014d34141aa8d9
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-03-29 23:26:04 -0400

    updating preamble, downloading and saving raw data

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,51 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Data: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Downloads and saves the data from Wordbank (an open database of children's vocabulary development)
+# Author: Aliyah Maxine Ramos
+# Data: 29 March 2023
+# Contact: aliyahmaxine.ramos@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need to know where to get Children Vocabulary Development data.
+# Any other information needed? None.
 
 
 #### Workspace setup ####
-library(opendatatoronto)
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
+# read in the datasets
 
+# by child summary raw data
+raw_bychildsummary_data <- 
+  read_csv(
+    file = "wordbank_by-child_summary_data.csv"
+  )
+
+# by word (in American Sign Language) produces by child raw data
+raw_byASLwordproduce_data <- 
+  read_csv(
+    file = "wordbank_by-word-produce_ASL_summary_data.csv"
+  )
+
+# by word (in American Sign Language) understood by child raw data
+raw_byASLwordunderstand_data <- 
+  read_csv(
+    file = "wordbank_by-word-understand_ASL_summary_data.csv"
+  )
+
+# full child by word (in American Sign Language) raw data
+raw_childbyASLword_data <- 
+  read_csv(
+    file = "wordbank_full-child-by-word_ASL_data.csv"
+  )
 
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
+write_csv(raw_bychildsummary_data, "inputs/data/raw_bychildsummary_data.csv") 
 
+write_csv(raw_byASLwordproduce_data, "inputs/data/raw_byASLwordproduce_data.csv") 
+
+write_csv(raw_byASLwordunderstand_data, "inputs/data/raw_byASLwordunderstand_data.csv") 
+
+write_csv(raw_childbyASLword_data, "inputs/data/raw_childbyASLword_data.csv") 
          
