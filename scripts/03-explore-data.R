e67dc2e0e3dcc1bb4d3133549d05912fbc042d70
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-03-30 11:56:43 -0400

    add explore data script to understand the data

---
+++ scripts/03-explore-data.R
@@ -0,0 +1,15 @@
+#### Preamble ####
+# Purpose: Explore and understand the dataset by creating data visualizations.
+# Author: Aliyah Maxine Ramos
+# Data: 30 March 2023
+# Contact: aliyahmaxine.ramos@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: Need to know where to get Children Vocabulary Development data.
+# Any other information needed? None.
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Explore data ####
