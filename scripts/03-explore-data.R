75d75c3f4946a2c9f3267a0f38d646419026dd93
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-04-15 19:48:58 -0400

    explored data visualizations for variables of interest

--- scripts/03-explore-data.R
+++ scripts/03-explore-data.R
@@ -10,6 +10,97 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Explore data ####
+### exploring the variables of interest ###
+
+# 1 ## each child's by word value ##
+cleaned_vocab_data |>
+  ggplot(mapping = aes(x = child_id, fill = value)) +
+  geom_bar() +
+  labs(
+    x = "Child ID",
+    y = "Number of Observations",
+    fill = "Ability",
+    title = "Children's Ability to Understand/Produce ASL Words"
+  )
+
+
+# 2 ## each child's amount of produced words ##
+produced_ASL_words <-
+  cleaned_vocab_data |>
+  filter(value == "produces")
+
+produced_ASL_words |>
+  ggplot(mapping = aes(x = child_id)) +
+  geom_bar(fill = "purple") +
+  labs(
+    x = "Child ID",
+    y = "Number of ASL Words Produced",
+    title = "Children's Ability to Produce ASL Words"
+  )
+
+
+# 3 ## each child's amount of produced action words ##
+produced_action_words <-
+  cleaned_vocab_data |>
+    filter(value == "produces", category == "action_words")
+
+produced_action_words |>
+  ggplot(mapping = aes(x = child_id)) +
+  geom_bar(fill = "blue") +
+  labs(
+    x = "Child ID",
+    y = "Number of Action Words Produced",
+    title = "Children's Ability to Produce ASL Action Words"
+  )
+
+
+# 4 ## each child's amount of produced time words ##
+produced_time_words <-
+  cleaned_vocab_data |>
+    filter(value == "produces", category == "time_words")
+
+produced_time_words |>
+  ggplot(mapping = aes(x = child_id)) +
+  geom_bar(fill = "blue") +
+  labs(
+    x = "Child ID",
+    y = "Number of Time Words Produced",
+    title = "Children's Ability to Produce ASL Time Words"
+  )
+
+
+# 5 ## each child's amount of produced question words ##
+produced_question_words <-
+  cleaned_vocab_data |>
+  filter(value == "produces", category == "question_words")
+
+produced_question_words |>
+  ggplot(mapping = aes(x = child_id)) +
+  geom_bar(fill = "blue") +
+  labs(
+    x = "Child ID",
+    y = "Number of Time Question Produced",
+    title = "Children's Ability to Produce ASL Question Words"
+  )
+
+
+# 6 ## each child's amount of produced descriptive words ##
+produced_descriptive_words <-
+  cleaned_vocab_data |>
+  filter(value == "produces", category == "descriptive_words")
+
+produced_descriptive_words |>
+  ggplot(mapping = aes(x = child_id)) +
+  geom_bar(fill = "blue") +
+  labs(
+    x = "Child ID",
+    y = "Number of Time Descriptive Produced",
+    title = "Children's Ability to Produce ASL Descriptive Words"
+  )
+
+
+
+### MODEL ###
