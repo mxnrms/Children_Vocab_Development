8c52a906a4ff09c6816011190fd66b5cf8571a24
Aliyah Maxine Ramos <aliyahmaxine.ramos@mail.utoronto.ca>
2023-04-17 20:40:52 -0400

    further data cleaning (added mutate, rename, and arrange functions)

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -19,13 +19,37 @@ raw_childbyASLword_data <- read_csv("inputs/data/raw_childbyASLword_data.csv")
 ### select variables of interest ###
 cleaned_vocab_data <-
   raw_childbyASLword_data |>
-    select(child_id, age, item_definition, category, value)
+  select(child_id, age, item_definition, category, value) |>
+  mutate(
+    production_ability = case_when(
+      endsWith(value, "produces") ~ "yes",
+      endsWith(value, "understands") ~ "no",
+      endsWith(value, "yes") ~ "yes",
+      endsWith(value, "no") ~ "no"
+    )
+  ) |>
+  mutate(
+    comprehension_ability = case_when(
+      endsWith(value, "produces") ~ "no",
+      endsWith(value, "understands") ~ "yes",
+      endsWith(value, "yes") ~ "yes",
+      endsWith(value, "no") ~ "no"
+    )
+  ) |>
+  mutate(
+    value = NULL
+  ) |>
+  rename(
+    age_in_months = age,
+    vocabulary_item_definition = item_definition,
+    item_category = category
+  ) |>
+  arrange(
+    age_in_months
+  )
+
 
 ### renaming values in variables to be more comprehensible ###
-## renaming value: 'yes' under variable: value ##
-cleaned_vocab_data$value [cleaned_vocab_data$value == "yes"] <- "understands & produces"
-cleaned_vocab_data$value [cleaned_vocab_data$value == "no"] <- "neither"
-
 ## renaming child id to simple values under variable: child_id ##
 cleaned_vocab_data$child_id [cleaned_vocab_data$child_id == 58253] <- 1
 cleaned_vocab_data$child_id [cleaned_vocab_data$child_id == 58250] <- 2
